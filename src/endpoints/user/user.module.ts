cd841428d451fe0c13571ecf9e194b393e9c6839
Jared Bada <official.jared.bada@gmail.com>
2024-10-06 20:35:52 +0300

    membership repo fixes

--- src/endpoints/user/user.module.ts
+++ src/endpoints/user/user.module.ts
@@ -10,9 +10,11 @@ import { GroupService } from '../group/group.service';
 import { Child } from './entities/child.entity';
 import { Spouse } from './entities/spouse.entity';
 import { User } from './entities/user.entity';
+import { Membership } from '../membership/membership.entity';
+import { Group } from '../group/group.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Spouse, Child])],
+  imports: [TypeOrmModule.forFeature([User, Spouse, Child, Membership, Group])],
   controllers: [UserController],
   providers: [UserService, MembershipService, GroupService],
 })
