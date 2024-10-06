3123d8a406be441e8744966d005c801102909ab1
Jared Bada <official.jared.bada@gmail.com>
2024-10-06 21:15:11 +0300

    cors;

--- src/main.ts
+++ src/main.ts
@@ -19,9 +19,9 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe());
 
-  if (process.env.NODE_ENV === 'development') {
+  // if (process.env.NODE_ENV === 'development') {
     app.enableCors();
-  }
+  // }
 
   await app.listen(PORT);
 
